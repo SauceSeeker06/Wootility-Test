6110436e826dc9eb6df19d935fb986ad16c8b8db
SauceSeeker <furiousbrooks@gmail.com>
2025-11-21 10:50:06 -0500

    initial commit

---
+++ package.nix
@@ -0,0 +1,20 @@
+{ stdenv, lib}:
+
+stdenv.mkDerivation rec {
+  pname = "Wootility-Test";
+  version = "0.0.1";
+
+  src = fetchFromGitHub {
+    owner = "SauceSeeker";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "0000000000000000000000000000000000000000000000000000";
+  };
+
+  meta = with lib; {
+    description = "My C application";
+    homepage = "https://github.com/github-owner/${pname}";
+    license = licenses.mit;
+    platforms = platforms.unix;
+  };
+}
