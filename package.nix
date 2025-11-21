a32962b00ac9b77d617b2db226a64697bd624911
SauceSeeker06 <furiousbrooks@gmail.com>
2025-11-21 12:38:05 -0500

    test

--- package.nix
+++ package.nix
@@ -1,20 +1,64 @@
-{ stdenv, lib, git}:
+{ 
+  stdenv, 
+  lib, 
+  fetchurl, 
+  appimageTools, 
+  makeWrapper
+}:
 
-stdenv.mkDerivation rec {
-  pname = "Wootility-Test";
-  version = "0.0.1";
 
-  src = fetchFromGitHub {
-    owner = "SauceSeeker06";
-    repo = https://github.com/${owner}/${pname};
-    rev = "v${version}";
-    sha256 = "0000000000000000000000000000000000000000000000000000";
+let
+  pname = "wootility";
+  version = "5.1.2";
+  src = fetchurl {
+    url = "https://api.wooting.io/public/${pname}/download?os=linux&version=${version}";
+    sha256 = "sha256-JcVyuilhy1qjXyIeniXZ0s4qxXr/4wLXrXgTTxjCkBk=";
   };
 
-  meta = with lib; {
-    description = "My C application";
-    homepage = "https://github.com/github-owner/${pname}";
-    license = licenses.mit;
-    platforms = platforms.unix;
-  };
-}
+
+in
+  appimageTools.wrapType2 {
+    inherit pname version src;
+
+    nativeBuildInputs = [ makeWrapper ];
+
+    extraInstallCommands =
+      let
+        contents = appimageTools.extract { inherit pname version src; };
+      in
+      ''
+        wrapProgram $out/bin/wootility \
+          --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}"
+
+        install -Dm444 ${contents}/wootility.desktop -t $out/share/applications
+        install -Dm444 ${contents}/wootility.png -t $out/share/pixmaps
+        install -Dpm644 $src etc/udev/rules.d/70-wooting.rules
+        substituteInPlace $out/share/applications/wootility.desktop \
+          --replace-fail 'Exec=AppRun --no-sandbox' 'Exec=wootility'
+      '';
+
+    profile = ''
+      export LC_ALL=C.UTF-8
+    '';
+
+    services.udev.packages = [
+      wootingRules
+  ];
+
+    extraPkgs =
+      pkgs: with pkgs; [
+        xorg.libxkbfile
+      ];
+
+    meta = {
+      homepage = "https://wooting.io/wootility";
+      description = "Customization and management software for Wooting keyboards";
+      platforms = lib.platforms.linux;
+      license = lib.licenses.free;
+      maintainers = with lib.maintainers; [
+        sodiboo
+        returntoreality
+      ];
+      mainProgram = "wootility";
+    };
+  }
